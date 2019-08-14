00ab3d265b41852db6912ecfbce6b46dd26bd68a
ArmenHarutyunyan1993 <armen.harutyunyan1126@gmail.com>
2019-08-14 10:19:34 +0400

    55

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,11 +5,11 @@ import userPhoto from '../../assets/images/_Bearded_Man-17-512.png';
 
 class Users extends React.Component{
 
-    constructor (props) {
-        super(props);
-            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-                this.props.setUsers(response.data.items);
-            });
+
+    componentDidMount() {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            this.props.setUsers(response.data.items);
+        });
     }
 
 
