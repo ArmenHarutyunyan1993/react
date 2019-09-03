3b9fcefcfca0da7874dafbec0fb053cda638090b
ArmenHarutyunyan1993 <armen.harutyunyan1126@gmail.com>
2019-09-03 14:52:16 +0400

    58 loader

---
+++ src/components/common/PreLoader/Preloader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import preLoader from "../../../assets/images/loader.gif";
+
+let PreLoader = (props) => {
+    return (
+        <div>
+            <img src={preLoader}/>
+        </div>
+    )
+};
+
+export default PreLoader;
